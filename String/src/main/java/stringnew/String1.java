aa30a7e5f11fcb2552db61bd266d5566b3b5246d
user <user@DESKTOP-ISDQQNT>
2023-09-13 12:12:22 +0530

    String welcome

---
+++ String/src/main/java/stringnew/String1.java
@@ -0,0 +1,13 @@
+package stringnew;
+
+public class String1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String S = "welcome";
+String S1 = new String("Sana");
+System.out.println(S);
+System.out.println(S1);
+}
+
+}
