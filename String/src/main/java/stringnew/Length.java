095146664cf1cdb36b6fbeda81937f97eb3dc7bc
user <user@DESKTOP-ISDQQNT>
2023-09-13 12:45:26 +0530

    String methods

---
+++ String/src/main/java/stringnew/Length.java
@@ -0,0 +1,11 @@
+package stringnew;
+
+public class Length {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String S = "Hello";
+System.out.println(S.length());
+	}
+
+}
